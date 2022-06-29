4b217e8155289a8a1d8985f70d561803ad713773
Arash Bordbar <bordbar36@yahoo.com>
2022-06-29 22:25:22 +0430

    add sql script for startup

---
+++ stratup.sql
@@ -0,0 +1,5 @@
+INSERT INTO Account_city (id, create_at, updated_at, City_name)
+VALUES (1,'2022-06-29 19:34:28.403120','2022-06-29 19:34:28.403120','تهران');
+
+INSERT INTO Account_unit (id, create_at, updated_at, Uity_name)
+VALUES (1,'2022-06-29 19:34:28.403120','2022-06-29 19:34:28.403120','اورژانس');
